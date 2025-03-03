29996fa2c50cab4754c37f3954f7962025f93c9d
danzin <excl.d.k@gmail.com>
2025-03-03 16:49:41 +0200

    fix: name of local db

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -6,7 +6,7 @@ export class DatabaseConfig {
   private dbUri: string;
 
   constructor() {
-    this.dbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
+    this.dbUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/image-app';
   }
 
   public async connect(): Promise<void> {
