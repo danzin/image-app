25dd2f283b4ba0f5cdfe5b01ee219e2d0c14d2ba
danzin <excl.d.k@gmail.com>
2025-03-11 18:06:21 +0200

    ImageCard reworked with DaisyUI

--- frontend/src/components/ImageCard.tsx
+++ frontend/src/components/ImageCard.tsx
@@ -1,42 +1,31 @@
 import React from 'react';
-import { Card, CardMedia, Typography, Box } from '@mui/material';
 import { ImageCardProps } from '../types';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 const ImageCard: React.FC<ImageCardProps> = ({ image, onClick }) => {
   const fullImageUrl = image.url.startsWith('http') ? image.url : `${BASE_URL}${image.url}`;
   return (
-    <Card
-      onClick={() => onClick(image)}
-      sx={{
-        width: '100%',
-        borderRadius: '8px',
-        overflow: 'hidden',
-        boxShadow: 3,
-        transition: 'transform 0.2s ease-in-out',
-        cursor: 'pointer',
-        '&:hover': { transform: 'scale(1.02)' },
-      }}
-    >
-      <CardMedia
-        component="img"
-        image={fullImageUrl}
-        alt={`Gallery item ${image.id}`}
-        sx={{
-          width: '100%',
-          objectFit: 'cover',
-          maxHeight: '600px',
-        }}
-      />
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', p: 2 }}>
-        <Typography variant="body2" color="text.secondary">
-          {new Date(image.createdAt).toLocaleDateString()}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {image.likes} ❤️
-        </Typography>
-      </Box>
-    </Card>
+    <>
+    <div className="card bg-base-100 w-[100%]
+                    rounded-lg overflow-hidden
+                    transition-transform duration-200
+                    ease-in-out cursor-pointer
+                    hover:scale-105 shadow-sm"
+                    onClick={() => onClick(image)}>
+      <figure>
+        <img
+          src={fullImageUrl}
+          alt={image.id} />
+      </figure>
+      <div className="card-body p-3">
+          <div className="card-actions justify-between">
+            <div className=''>{new Date(image.createdAt).toLocaleDateString()}</div>
+            <div className="badge badge-outline"> {image.likes} ❤️
+            </div>
+          </div>
+      </div>
+    </div>
+  </>
   );
 };
 
