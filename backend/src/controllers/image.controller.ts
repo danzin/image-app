831f468a3907ab4a008ef3303c16c96e167e54fb
danzin <excl.d.k@gmail.com>
2025-05-17 15:48:35 +0300

    fix: Local storage uploads uses userid instead of username

--- backend/src/controllers/image.controller.ts
+++ backend/src/controllers/image.controller.ts
@@ -1,78 +1,96 @@
-import { Request, Response, NextFunction } from 'express';
-import { ImageService } from '../services/image.service';
-import { createError } from '../utils/errors';
-import { errorLogger } from '../utils/winston';
-import { inject, injectable } from 'tsyringe';
+import { Request, Response, NextFunction } from "express";
+import { ImageService } from "../services/image.service";
+import { createError } from "../utils/errors";
+import { errorLogger } from "../utils/winston";
+import { inject, injectable } from "tsyringe";
 
-/**  
+/**
  * When using Dependency Injection in Express, there's a common
  * issue with route handles and `this` binding. When Express calls the route handlers,
  * it changes the context of `this`. So when I initialize the dependncy inside the constructor
  * like this.userService = userService, `this` context is lost and this.userService is undefined.
- * 
- * 2 possible fixes: 
+ *
+ * 2 possible fixes:
  *  1 - manually bind all methods that will be used as route handlers:
  *     - this.register = this.register.bind(this);
  *     - etc etc, for every single method
- *  2 - user arrow functions, which automatically bind `this` and it doesn't get lost. 
+ *  2 - user arrow functions, which automatically bind `this` and it doesn't get lost.
  */
 
 @injectable()
 export class ImageController {
-
   constructor(
-    @inject('ImageService') private readonly imageService: ImageService
+    @inject("ImageService") private readonly imageService: ImageService
   ) {}
 
-
-  uploadImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  uploadImage = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     try {
       const { decodedUser, file } = req;
 
-      console.log(req.body.tags)
+      console.log(req.body.tags);
       const tags = JSON.parse(req.body.tags);
 
       if (!file) {
-        throw createError('ValidationError', 'No file uploaded');
+        throw createError("ValidationError", "No file uploaded");
       }
 
-      const result = await this.imageService.uploadImage(decodedUser.id, file.buffer, tags);
+      const result = await this.imageService.uploadImage(
+        decodedUser.id,
+        file.buffer,
+        tags
+      );
       res.status(201).json(result);
     } catch (error) {
       next(createError(error.name, error.message));
     }
-  }
+  };
 
-  getImages = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+  getImages = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     const page = parseInt(req.query.page as string) || 1;
     const limit = parseInt(req.query.limit as string) || 9;
     try {
       const images = await this.imageService.getImages(page, limit);
-      res.header('Access-Control-Allow-Origin', 'http://localhost:5173');  //specific origin
-      res.header('Access-Control-Allow-Credentials', 'true');  //allow credentials
+      res.header("Access-Control-Allow-Origin", "http://localhost:5173"); //specific origin
+      res.header("Access-Control-Allow-Credentials", "true"); //allow credentials
       res.json(images);
     } catch (error) {
       next(createError(error.name, error.message));
     }
-  }
+  };
 
-  getUserImages = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const {id} = req.params;
-   
+  getUserImages = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    const { id } = req.params;
+
     const page = parseInt(req.query.page as string) || 1;
     const limit = parseInt(req.query.limit as string) || 10;
-    console.log('ID of getUserImages: ', id)
+    console.log("ID of getUserImages: ", id);
     try {
       const images = await this.imageService.getUserImages(id, page, limit);
       console.log(`images of user ${id}: ${images}`);
       res.json(images);
     } catch (error) {
       errorLogger.error(error.stack);
-      next(createError('UnknownError', 'Failed to fetch images'))
+      next(createError("UnknownError", "Failed to fetch images"));
     }
-  }
+  };
 
-  getImageById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  getImageById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     try {
       const { id } = req.params;
       const result = await this.imageService.getImageById(id);
@@ -80,47 +98,59 @@ export class ImageController {
     } catch (error) {
       next(createError(error.name, error.message));
     }
-  }
+  };
 
-  searchByTags = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+  searchByTags = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     try {
-      const { tags } = req.query; 
+      const { tags } = req.query;
       const page = parseInt(req.query.page as string) || 1;
       const limit = parseInt(req.query.limit as string) || 10;
-      console.log('tags in the controller:', tags)
+      console.log("tags in the controller:", tags);
       // Will leave empty tags allowed for now
-      const tagArray = tags 
-        ? (tags as string).split(',').filter(tag => tag.trim() !== '')
+      const tagArray = tags
+        ? (tags as string).split(",").filter((tag) => tag.trim() !== "")
         : [];
-      console.log(tagArray)
+      console.log(tagArray);
       // Call the service method
-      const result = await this.imageService.searchByTags(tagArray, page, limit);
+      const result = await this.imageService.searchByTags(
+        tagArray,
+        page,
+        limit
+      );
       res.status(200).json(result);
     } catch (error) {
       next(error);
     }
-  }
+  };
 
- 
-
-  deleteImage = async(req: Request, res: Response, next: NextFunction): Promise<void> =>  {
+  deleteImage = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     try {
       const { id } = req.params;
       const result = await this.imageService.deleteImage(id);
-      res.status(200).json(result); 
+      res.status(200).json(result);
     } catch (error) {
       next(createError(error.name, error.message));
     }
-  }
+  };
 
-  getTags = async(req: Request, res: Response, next: NextFunction): Promise<void> => {
+  getTags = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
     try {
       const result = await this.imageService.getTags();
-      res.json(result)
+      res.json(result);
     } catch (error) {
       next(createError(error.name, error.message));
-
     }
-  }
-
+  };
 }
--- backend/src/services/cloudinary.service.ts
+++ backend/src/services/cloudinary.service.ts
@@ -1,20 +1,26 @@
-import { v2 as cloudinary, UploadApiResponse, UploadApiErrorResponse, ConfigOptions } from 'cloudinary';
-import { createError } from '../utils/errors';
-import { CloudinaryDeleteResponse, CloudinaryResponse, DeletionResult } from '../types';
-import { inject, injectable } from 'tsyringe';
-import { IImageStorageService } from '../types/customImageStorage/imageStorage.types';
-
-
+import {
+  v2 as cloudinary,
+  UploadApiResponse,
+  UploadApiErrorResponse,
+  ConfigOptions,
+} from "cloudinary";
+import { createError } from "../utils/errors";
+import {
+  CloudinaryDeleteResponse,
+  CloudinaryResponse,
+  DeletionResult,
+} from "../types";
+import { inject, injectable } from "tsyringe";
+import { IImageStorageService } from "../types/customImageStorage/imageStorage.types";
 
 @injectable()
 export class CloudinaryService implements IImageStorageService {
   constructor() {
     cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET,
-      });
-
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
   }
   private extractPublicId(url: string): string | null {
     const regex = /\/(?:v\d+\/)?([^\/]+)\.[a-zA-Z]+$/;
@@ -22,53 +28,57 @@ export class CloudinaryService implements IImageStorageService {
     return matches ? matches[1] : null;
   }
 
-
-  async uploadImage(file: Buffer, username: string): Promise<{url: string, publicId: string}> {
+  async uploadImage(
+    file: Buffer,
+    userId: string
+  ): Promise<{ url: string; publicId: string }> {
     try {
       const result = await cloudinary.uploader.upload(
-        `data:image/png;base64,${file.toString('base64')}`, // Convert Buffer to base64
-        { folder: username }
+        `data:image/png;base64,${file.toString("base64")}`, // Convert Buffer to base64
+        { folder: userId }
       );
-  
+
       return {
         url: result.secure_url,
         publicId: result.public_id,
       };
     } catch (error) {
       throw createError(error.name, error.message);
-
     }
   }
-  
-  async deleteAssetByUrl(username: string, url: string): Promise<{ result: string }> {
+
+  async deleteAssetByUrl(
+    username: string,
+    url: string
+  ): Promise<{ result: string }> {
     const publicId = this.extractPublicId(url);
     if (!publicId) {
-      throw new Error('Invalid URL format');
+      throw new Error("Invalid URL format");
     }
-  
+
     try {
       console.log("URL of image about to delete:", url);
       const assetPath = `${username}/${publicId}`;
       const result = await cloudinary.uploader.destroy(assetPath);
       console.log(result);
-  
+
       // Return onlythe necessary fields to make sure object doesn't get polluted with unserializable data
       return { result: result.result }; // Only include the result field
     } catch (error) {
-      console.error('Error deleting asset:', error);
-      throw createError('StorageError', error.message);
+      console.error("Error deleting asset:", error);
+      throw createError("StorageError", error.message);
     }
   }
 
-      //deletes image by public Id
-      async deleteImage(publicId: string): Promise<void> {
-        try {
-          cloudinary.uploader.destroy(publicId)
-        } catch (error) {
-          console.error(error);
-          throw createError(error.name, error.message);
-        };
-      };
+  //deletes image by public Id
+  async deleteImage(publicId: string): Promise<void> {
+    try {
+      cloudinary.uploader.destroy(publicId);
+    } catch (error) {
+      console.error(error);
+      throw createError(error.name, error.message);
+    }
+  }
 
   //deletes lots of images with username prefix
   async deleteMany(username: string): Promise<DeletionResult> {
@@ -77,44 +87,40 @@ export class CloudinaryService implements IImageStorageService {
       return this.processDeleteResponse(result);
     } catch (error) {
       return {
-        result: 'error',
-        message: error.message || 'Error deleting cloudinary resources'
+        result: "error",
+        message: error.message || "Error deleting cloudinary resources",
       };
     }
   }
 
-    /**
-     * @processDeleteResponse method accepts the response from the resolved promise returned by cloudinary.api.delete_resources_by_prefix 
-     * Object.value() converts the response object's deleted_counts into an array
-     * of the values of the object. The object in this case is {'username/imagename': { original: x, derived: 0 }}
-     * The array looks like this: 
-     * [
-     *  { original: 1, derived: 0 }
-     * ]
-     * .some() is an array method that returns true if AT LEAST ONE
-     * element in the array satisfies the condition in the callback function.
-     * The count parameter in the callback of `some()` is each object like { original: 1, derived: 0 }.
-     * and the condition is > 0 
-     * That way there are no unnecessary errors from successful deletions
-    */
-  private processDeleteResponse(response: CloudinaryDeleteResponse): DeletionResult {
-
-  
+  /**
+   * @processDeleteResponse method accepts the response from the resolved promise returned by cloudinary.api.delete_resources_by_prefix
+   * Object.value() converts the response object's deleted_counts into an array
+   * of the values of the object. The object in this case is {'username/imagename': { original: x, derived: 0 }}
+   * The array looks like this:
+   * [
+   *  { original: 1, derived: 0 }
+   * ]
+   * .some() is an array method that returns true if AT LEAST ONE
+   * element in the array satisfies the condition in the callback function.
+   * The count parameter in the callback of `some()` is each object like { original: 1, derived: 0 }.
+   * and the condition is > 0
+   * That way there are no unnecessary errors from successful deletions
+   */
+  private processDeleteResponse(
+    response: CloudinaryDeleteResponse
+  ): DeletionResult {
     const hasSuccessfulDeletions = Object.values(response.deleted_counts).some(
-      count => count.original > 0
+      (count) => count.original > 0
     );
-  
+
     if (hasSuccessfulDeletions) {
-      return { result: 'ok' };
+      return { result: "ok" };
     }
 
     return {
-      result: 'error',
-      message: 'No resources were deleted'
+      result: "error",
+      message: "No resources were deleted",
     };
   }
-
-
-
-  
 }
