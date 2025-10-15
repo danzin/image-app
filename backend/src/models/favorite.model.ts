f1b67132469fa0d1f0c75d8f714727ed8eb0e8e8
danzin <excl.d.k@gmail.com>
2025-10-15 10:35:38 +0300

    refactor: improved indexing and aggregation pipeline efficiency for generating user feeds

--- backend/src/models/favorite.model.ts
+++ backend/src/models/favorite.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { IFavorite } from "types/index";
 
 const favoriteSchema = new Schema<IFavorite>(
@@ -18,7 +18,7 @@ const favoriteSchema = new Schema<IFavorite>(
 );
 
 favoriteSchema.index({ userId: 1, imageId: 1 }, { unique: true }); // compound index to ensure a user can only favorite an image once
-favoriteSchema.index({ userId: 1, createdAt: -1 }); // index to efficiently query for a user's favorites, sorted by most recent
+favoriteSchema.index({ userId: 1, createdAt: -1 }); // compound index to efficiently query for a user's favorites, sorted by most recent
 
 const Favorite = mongoose.model<IFavorite>("Favorite", favoriteSchema);
 export default Favorite;
