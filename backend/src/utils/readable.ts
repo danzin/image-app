292847547935b23dd5228b9c144885be9cb9bf1e
danzin <excl.d.k@gmail.com>
2025-11-28 17:25:21 +0200

    refactor: removed redundant method

--- backend/src/utils/readable.ts
+++
@@ -1,9 +0,0 @@
-import { Readable } from 'stream';
-
-//Convert buffer to readable stream
-export function bufferToStream(buffer: Buffer): Readable {
-  const stream = new Readable();
-  stream.push(buffer);
-  stream.push(null); //Signal end of the stream
-  return stream;
-}
