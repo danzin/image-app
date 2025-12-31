2faf5500289cd06a08e58c3aa345127d78ba4779
danzin <excl.d.k@gmail.com>
2025-12-31 16:10:48 +0200

    building communities

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -22,3 +22,5 @@ export * from "./customPosts/postLike.types";
 export * from "./customFeeds/feed.types";
 export * from "./customTags/tags.types";
 export * from "./customAuth/auth.types";
+export * from "./customCommunities/community.type";
+export * from "./customCommunities/communityMemeber.type";
