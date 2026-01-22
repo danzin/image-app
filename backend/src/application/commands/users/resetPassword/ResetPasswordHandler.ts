9c8571e52a266a703f44fae97c8e2d8afe6ca8ca
danzin <excl.d.k@gmail.com>
2026-01-22 12:38:02 +0200

    proper read/write user repo implementation in the reset password CQRS flow

--- backend/src/application/commands/users/resetPassword/ResetPasswordHandler.ts
+++ backend/src/application/commands/users/resetPassword/ResetPasswordHandler.ts
@@ -1,21 +1,24 @@
 import { inject, injectable } from "tsyringe";
 import { ICommandHandler } from "../../../common/interfaces/command-handler.interface";
 import { ResetPasswordCommand } from "./ResetPasswordCommand";
-import { UserRepository } from "../../../../repositories/user.repository";
 import { createError } from "../../../../utils/errors";
+import { IUserReadRepository, IUserWriteRepository } from "repositories/interfaces";
 
 @injectable()
 export class ResetPasswordHandler implements ICommandHandler<ResetPasswordCommand, void> {
-	constructor(@inject("UserRepository") private readonly userRepository: UserRepository) {}
+	constructor(
+		@inject("UserReadRepository") private readonly userReadRepository: IUserReadRepository,
+		@inject("UserWriteRepository") private readonly userWriteRepository: IUserWriteRepository,
+	) {}
 
 	async execute(command: ResetPasswordCommand): Promise<void> {
-		const user = await this.userRepository.findByResetToken(command.token);
+		const user = await this.userReadRepository.findByResetToken(command.token);
 
 		if (!user) {
 			throw createError("ValidationError", "Invalid or expired reset token");
 		}
 
-		await this.userRepository.update(user.id, {
+		await this.userWriteRepository.update(user.id, {
 			$set: { password: command.newPassword },
 			$unset: { resetToken: 1, resetTokenExpires: 1 },
 		});
